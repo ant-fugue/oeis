aec09d5a51caf8e114d8d2af9340bb0356e3c1df
searchingforapuppy <ebisulove2@gmail.com>
2020-11-16 17:51:01 +0900

    Restructure directories Each sequence has its corresponding directory In JavaScript, some of basic functions like iota, which returns a list of natural number, is defined in qjs.js Other js programs import qjs.js and use those functions

---
+++ A005117/A005117.js
@@ -0,0 +1,31 @@
+const QJS = require('./qjs')
+const qjs = new QJS()
+
+const isSquareFree = (num) => {
+  for (let i = 2; i < num + 1; i++) {
+    if (num % i ** 2 === 0) return false;
+  }
+  return true;
+}
+
+const squareFreeList = (start, end) => qjs.iota(start, end).filter(elem => isSquareFree(elem));
+// const countSquareFree = (start, end) => squareFreeList(start, end).reduce((accumulator, currentValue) => accumulator + currentValue);
+const countSquareFree = (start, end) => {
+  let counter = 0;
+  squareFreeList(start, end).forEach(elem => counter++);
+  return counter;
+}
+
+console.log(squareFreeList(1, 145));
+
+console.log()
+// console.log(squareFreeList(1e12,1e12+145));
+// console.log(countSquareFree(1e12,1e12+145));
+
+const divSum = (num) => {
+  let divisors = [];
+  for (let i = 1; i < num+1; i++) {
+    if (num % i === 0) divisors.push(i);
+  }
+  return divisors.reduce((a, c) => a + c);
+}
---
+++ qjs.js
@@ -0,0 +1,13 @@
+module.exports = class QJS {
+  iota(min, max) {
+    let arr = []
+    for (let i = min; i < max+1; i++) {
+      arr.push(i)
+    }
+    return arr
+  }
+}
+
+
+
+
