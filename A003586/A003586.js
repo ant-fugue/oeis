97778d729e7c75d2cb945db6de4e4c7a17e9dc9b
searchingforapuppy <ebisulove2@gmail.com>
2020-11-25 10:10:49 +0900

    add test for A003586.js
    add eslint configuration
    
    revised test message in A003586.test.js
    
    formatted codes

--- A003586/A003586.js
+++ A003586/A003586.js
@@ -1,7 +1,11 @@
 const QJS = require('../qjs')
 const qjs = new QJS()
 
-const f = (num) => {
+// see detail of this sequence at:
+// https://oeis.org/A003586
+// https://en.wikipedia.org/wiki/Smooth_number
+
+const isThreeSmoothNum = (num) => {
   while (num !== 1) {
     if (num % 2 === 0) num = num / 2
     else if (num % 3 === 0) num = num / 3
@@ -17,15 +21,14 @@ const g = (num) => {
     if (num % 2 === 0) {
       num = num / 2
       a++
-    }
-    else if (num % 3 === 0) {
+    } else if (num % 3 === 0) {
       num = num / 3
       b++
-    }
-    else return false
+    } else return false
   }
   return `${2 ** a * 3 ** b} = 2^${a} * 3^${b}`
 }
-console.log(qjs.iota(1, 100).filter(elem => f(elem)))
-console.log(qjs.iota(1, 100).map(elem => g(elem)).filter(e => e !== false))
 
+// console.log(qjs.iota(1, 100).map(elem => g(elem)).filter(e => e !== false))
+
+module.exports = isThreeSmoothNum
--- A005117/A005117.js
+++ A005117/A005117.js
@@ -1,31 +1,32 @@
-const QJS = require('./qjs')
+const QJS = require('../qjs')
 const qjs = new QJS()
 
 const isSquareFree = (num) => {
   for (let i = 2; i < num + 1; i++) {
-    if (num % i ** 2 === 0) return false;
+    if (num % i ** 2 === 0) return false
   }
-  return true;
+  return true
 }
 
-const squareFreeList = (start, end) => qjs.iota(start, end).filter(elem => isSquareFree(elem));
+const squareFreeList = (start, end) =>
+  qjs.iota(start, end).filter((elem) => isSquareFree(elem))
 // const countSquareFree = (start, end) => squareFreeList(start, end).reduce((accumulator, currentValue) => accumulator + currentValue);
 const countSquareFree = (start, end) => {
-  let counter = 0;
-  squareFreeList(start, end).forEach(elem => counter++);
-  return counter;
+  let counter = 0
+  squareFreeList(start, end).forEach((elem) => counter++)
+  return counter
 }
 
-console.log(squareFreeList(1, 145));
+console.log(squareFreeList(1, 145))
 
 console.log()
 // console.log(squareFreeList(1e12,1e12+145));
 // console.log(countSquareFree(1e12,1e12+145));
 
 const divSum = (num) => {
-  let divisors = [];
-  for (let i = 1; i < num+1; i++) {
-    if (num % i === 0) divisors.push(i);
+  let divisors = []
+  for (let i = 1; i < num + 1; i++) {
+    if (num % i === 0) divisors.push(i)
   }
-  return divisors.reduce((a, c) => a + c);
+  return divisors.reduce((a, c) => a + c)
 }
