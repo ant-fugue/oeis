319507618d04f9883fdfb5ed3726f3f0db43acf4
searchingforapuppy <ebisulove2@gmail.com>
2021-05-24 17:35:11 +0900

    Update A001539.ts

--- JavaScript/src/sequences/A001539.ts
+++ JavaScript/src/sequences/A001539.ts
@@ -1,15 +1,14 @@
-import QJS from '../qjs'
-
-export const isA001539Num = (n: number): boolean | string => {
+export const isA001539Num = (n: number): boolean => {
   if (!Number.isInteger(n)) {
-    return 'input should be integer'
+    throw Error('input should be integer')
   }
   if (n % ((4 * n + 1) * (4 * n + 3))) return true
   else return false
 }
 
-export const generateA001539Num = (n: number) => (4 * n + 1) * (4 * n + 3)
-
-console.log(isA001539Num(0.1))
-console.log(isA001539Num(12))
-console.log(QJS.iota(1, 10).map((elem: number) => generateA001539Num(elem)))
+export const generateA001539Num = (n: number) => {
+  if (!Number.isInteger(n)) {
+    throw Error('input should be integer')
+  }
+  return (4 * n + 1) * (4 * n + 3)
+}
