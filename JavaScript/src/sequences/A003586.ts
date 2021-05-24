0ba62760c636e4d86fd629fa3d0ddb926cab404c
searchingforapuppy <ebisulove2@gmail.com>
2021-05-24 17:35:29 +0900

    Update A003586.ts

--- JavaScript/src/sequences/A003586.ts
+++ JavaScript/src/sequences/A003586.ts
@@ -1,11 +1,11 @@
-import QJS from '../qjs'
-const qjs = new QJS()
-
 // see detail of this sequence at:
 // https://oeis.org/A003586
 // https://en.wikipedia.org/wiki/Smooth_number
 
 export const isThreeSmoothNum = (num: number): boolean => {
+  if (!Number.isInteger(num) || num < 0) {
+    throw Error('the argument should be integer')
+  }
   while (num !== 1) {
     if (num % 2 === 0) num = num / 2
     else if (num % 3 === 0) num = num / 3
@@ -15,7 +15,10 @@ export const isThreeSmoothNum = (num: number): boolean => {
 }
 
 // show how each number of 3-smooth number is factorized
-const g = (num: number) => {
+const g = (num: number): string => {
+  if (!Number.isInteger(num) || num < 0) {
+    throw Error('the argument should be integer')
+  }
   let a = 0
   let b = 0
   while (num !== 1) {
@@ -25,9 +28,9 @@ const g = (num: number) => {
     } else if (num % 3 === 0) {
       num = num / 3
       b++
-    } else return false
+    } else {
+      return `${num} is not a 3-smooth number`
+    }
   }
   return `${2 ** a * 3 ** b} = 2^${a} * 3^${b}`
 }
-
-// console.log(qjs.iota(1, 100).map(elem => g(elem)).filter(e => e !== false))
