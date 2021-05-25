2b96dc91371d3ac39693c190ed102937640b2b84
searchingforapuppy <ebisulove2@gmail.com>
2021-05-25 21:44:22 +0900

    Modify A001539.test.ts

--- JavaScript/src/test/A001539.test.ts
+++ JavaScript/src/test/A001539.test.ts
@@ -1,11 +1,13 @@
-import { TestScheduler } from 'jest'
 import QJS from '../qjs.js'
-import A001539 from '../../JavaScript/sequences/A001539'
-const qjs = new QJS()
+import { isA001539Num } from '../sequences/A001539'
+import { generateA001539Num } from '../sequences/A001539'
+import assert from 'assert'
 
-test('first 20 elements of the sequence starting from 0', () => {
-  expect(qjs.iota(0, 19).map((elem) => A001539(elem))).toEqual([
+describe('first 20 elements of the sequence starting from 0', () => {
+  const actual = QJS.iota(0, 19).map((elem) => generateA001539Num(elem))
+  const expected = [
     3, 35, 99, 195, 323, 483, 675, 899, 1155, 1443, 1763, 2115, 2499, 2915,
     3363, 3843, 4355, 4899, 5475, 6083,
-  ])
+  ]
+  assert.deepStrictEqual(actual, expected)
 })
