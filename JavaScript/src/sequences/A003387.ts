00d8c0a962317c3ac4b171fab6003f0def401727
searchingforapuppy <ebisulove2@gmail.com>
2021-07-13 19:44:19 +0900

    Delete sequences temporarily which are not tested

--- JavaScript/src/sequences/A003387.ts
+++
@@ -1,8 +0,0 @@
-// const { TestScheduler } = require('jest')
-import QJS from '../qjs.js'
-// const isThreeSmoothNum = require('../../JavaScript/sequences/sequenceName')
-const qjs = new QJS()
-
-// const f = (num) => {
-
-// }
--- JavaScript/src/test/A003586.test.ts
+++
@@ -1,9 +0,0 @@
-import QJS from '../qjs.js'
-import { isThreeSmoothNum } from '../sequences/A003586'
-const qjs = new QJS()
-
-test('list of natural numbers whose prime factors are 2 or 3', () => {
-  expect(qjs.iota(1, 20).filter((elem) => isThreeSmoothNum(elem))).toEqual([
-    1, 2, 3, 4, 6, 8, 9, 12, 16, 18,
-  ])
-})
