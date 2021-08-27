f7ba40f38934bf88913ba26bcc22e51c77eba8a3
searchingforapuppy <ebisulove2@gmail.com>
2021-08-27 09:19:19 +0900

    Update A046559.test.ts to add a test case

--- oeis-ts/src/test/A046459.test.ts
+++ oeis-ts/src/test/A046459.test.ts
@@ -1,13 +1,14 @@
 import { isA046459Num } from '../sequences/A046459'
 
-// the sequence generation test
-// TODO the inclusion judging test
+// the inclusion judging test
 describe('isA046459Num function test', () => {
   it('returns true if the given argument is in the sequence', () => {
     expect(isA046459Num(0)).toBe(true)
     expect(isA046459Num(1)).toBe(true)
     expect(isA046459Num(17)).toBe(true)
+    expect(isA046459Num(37)).toBe(false)
   })
 })
 
+// TODO the sequence generation test
 // TODO the index extraction test
